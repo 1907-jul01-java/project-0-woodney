5da39246e8d8069e7aec6a3f9547585919918ac1
woodney <woodneyguerrier@gmail.com>
2019-07-12 22:54:40 -0500

    6th update

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,7 +4,7 @@ create table client
 (
     id serial primary key,
     username text not null,
-    password text not null
+    user_password text not null
 );
 
 create table bankAccount (
@@ -19,10 +19,12 @@ create table bankTransaction (
     transaction_type text not null,
     amount int not null,
     previous_balance float
-)
+);
 
 create table employee (
     employee_id serial primary key,
     employee_username text not null,
     employee_password text not null
 );
+
+insert into client(username,user_password) values ('woodneyg', 'password');
